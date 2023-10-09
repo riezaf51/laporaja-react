2664337f975b6e1c8512708a862a393b26d31d35
riezaf51 <riezaf51@gmail.com>
2023-10-09 09:35:17 +0700

    fix: disable credentials for axios

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = false;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
