6c01c5b920382aaebec47cf372d6ba197e46c0c6
riezaf51 <riezaf51@gmail.com>
2023-10-11 15:22:32 +0700

    refactor: forum page

--- src/Homepage/Sections/Forum.js
+++ src/Homepage/Sections/Forum.js
@@ -4,6 +4,7 @@ import Loading from '../../Components/Loading';
 import { API_URL } from '../../strings';
 import axios from 'axios';
 import ForumCard from '../../Components/ForumCard';
+import NoData from '../../Components/NoData';
 
 export default function Forum() {
     const [data, setData] = useState([]);
@@ -11,23 +12,23 @@ export default function Forum() {
     const [success, setSuccess] = useState(false);
 
     useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${API_URL}/api/laporan`);
+                const jsonData = response.data
+                console.log(response.data);
+                setData(jsonData);
+                setSuccess(true);
+                setLoading(false); // Set loading to false on error as well
+            } catch (error) {
+                console.error('Error fetching data: ' + error);
+                setLoading(false); // Set loading to false on error as well
+                // alert(error);
+            }
+        };
+
         fetchData();
     }, []);
-
-    const fetchData = async () => {
-        try {
-            const response = await axios.get(`${API_URL}/api/laporan`);
-            const jsonData = response.data
-            console.log(response.data);
-            setData(jsonData);
-            setSuccess(true);
-            setLoading(false); // Set loading to false on error as well
-        } catch (error) {
-            console.error('Error fetching data: ' + error);
-            setLoading(false); // Set loading to false on error as well
-            // alert(error);
-        }
-    };
 
     if (loading) {
         return (
@@ -38,8 +39,14 @@ export default function Forum() {
     if (!success) {
         return (
             <div className="container d-flex justify-content-center align-items-center vh-100">
-                Problem occured while fetching data
+                <h6>Problem occured while fetching data</h6>
             </div>
+        );
+    }
+
+    if (data.data.length === 0) {
+        return (
+            <NoData padded={true} />
         );
     }
 
