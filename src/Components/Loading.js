7b63699d01a38f7c141c285bc478d3051ea35cb2
riezaf51 <riezaf51@gmail.com>
2023-10-11 15:22:15 +0700

    refactor: loading component

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,9 +1,9 @@
 import loadingGif from "../Images/loading.gif";
 
-export default function Loading() {
+export default function Loading({ padded = true }) {
     return (
-        <div className="d-flex align-items-center justify-content-center min-vh-100">
+        <div className={"container d-flex justify-content-center align-items-center " + (padded && 'min-vh-100')} >
             <img height="50px" src={loadingGif} alt="Loading..." />
-        </div>
+        </div >
     );
 }
